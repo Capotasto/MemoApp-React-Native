7a62da339240879aa0aeee88a19f3344e1cd259f
Capotasto <rocker519804@gmail.com>
2020-03-20 21:47:26 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!2</Text>
+      <Button
+        title="Click Me"
+        onPress={() => { 
+          alert("アラート表示")
+          }
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
